30f32438fac4bcf9cc8f91b2fe4f00e713c64998
saltyJeff <saltyJeff@users.noreply.github.com>
2018-03-18 21:16:21 -0700

    reformed coder API

--- app/src/main/java/org/woodbridgehigh/healthid/HealthIdApi.java
+++ app/src/main/java/org/woodbridgehigh/healthid/HealthIdApi.java
@@ -4,14 +4,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 
+import org.woodbridgehigh.healthid.models.Patient;
+
 public class HealthIdApi {
 	private Context context;
 	public HealthIdApi(Context context) {
 		this.context = context;
 	}
-	public void printPatient(String patientStr) {
-		Bitmap textBmp = PersonBmpCoder.patientToTextBmp(patientStr);
-		Bitmap aztecBmp = PersonBmpCoder.patientToAztecBmp(patientStr);
+	public void printPatient(Patient patient) {
+		Bitmap textBmp = PersonBmpCoder.patientToTextBmp(patient);
+		Bitmap aztecBmp = PersonBmpCoder.patientToAztecBmp(patient);
 		Intent intent = new Intent(context, PrintActivity.class);
 		intent.putExtra("textBmp", textBmp);
 		intent.putExtra("aztecBmp", aztecBmp);
--- app/src/main/java/org/woodbridgehigh/healthid/PersonBmpCoder.java
+++ app/src/main/java/org/woodbridgehigh/healthid/PersonBmpCoder.java
@@ -7,21 +7,27 @@ import android.graphics.Bitmap;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import org.woodbridgehigh.healthid.models.Person;
+import org.woodbridgehigh.healthid.models.MinPatient;
+import org.woodbridgehigh.healthid.models.Patient;
 
 public class PersonBmpCoder {
 	private static Gson gson = new GsonBuilder().create();
-	public static Bitmap patientToTextBmp(String patientStr) {
+	public static Bitmap patientToTextBmp(Patient patient) {
 		return null;
 	}
-	public static Bitmap patientToAztecBmp(String patientStr) {
+	public static Bitmap patientToAztecBmp(Patient patient) {
 		return null;
 	}
 	public static String aztecToPatient(byte[] data) {
 		return null;
 	}
 	//utilities (you're welcome Alek)
-	private Person stringToPerson(String personStr) {
-		return gson.fromJson(personStr, Person.class);
+	private Patient stringToPatient(String minPatientStr) {
+		MinPatient minPatient = gson.fromJson(minPatientStr, MinPatient.class);
+		return Patient.fromMinPatient(minPatient);
+	}
+	private String patientToString(Patient patient) {
+		MinPatient minPatient = MinPatient.fromPatient(patient);
+		return gson.toJson(minPatient);
 	}
 }
